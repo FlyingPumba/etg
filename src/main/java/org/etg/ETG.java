9331490bf74f297ef3aa893532f9d223f91bff51
Ivan <iarcuschin@gmail.com>
2019-08-29 13:15:36 -0300

    Run espresso tests in ETG

--- src/main/java/org/etg/ETG.java
+++ src/main/java/org/etg/ETG.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.etg.espresso.codegen.TestCodeGenerator;
 import org.etg.mate.models.TestCase;
 import org.etg.mate.parser.TestCaseParser;
+import org.etg.utils.ProcessRunner;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -22,37 +24,93 @@ public class ETG {
         String filePath = args[0];
         String packageName = args[1];
         String testPackageName = args[2];
-        String outputFolderPath = args[3];
+        String rootProjectFolderPath = args[3];
+        String outputFolderPath = args[4];
 
         System.out.println("Working on file with path: " + filePath + " and package name: " + packageName);
 
         try {
-            String content = readFile(filePath, StandardCharsets.US_ASCII);
-
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode jsonNode = mapper.readTree(content);
-
-            List<TestCase> testCases = TestCaseParser.parseList(mapper, jsonNode);
+            List<TestCase> testCases = parseTestCases(filePath);
 
             TestCodeGenerator codeGenerator = new TestCodeGenerator(packageName, testPackageName);
             List<String> espressoTestCases = codeGenerator.getEspressoTestCases(testCases);
 
-            for (int i = 0; i < espressoTestCases.size(); i++) {
-                String testContent = espressoTestCases.get(i);
-                String outputFilePath = outputFolderPath + "TestCase" + i + ".java";
+            writeTestCases(outputFolderPath, espressoTestCases);
 
-                PrintWriter out = new PrintWriter(new FileOutputStream(outputFilePath), true);
-                out.print(testContent);
-                out.close();
-            }
-
-
-        } catch (IOException e) {
+            runTestCases(packageName, testPackageName, rootProjectFolderPath, outputFolderPath, espressoTestCases);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    static String readFile(String path, Charset encoding) throws IOException {
+    private static void runTestCases(String packageName, String testPackageName, String rootProjectFolderPath,
+                                     String outputFolderPath, List<String> espressoTestCases) throws Exception {
+        // compile tests
+        String compileCmd = String.format("%sgradlew -p %s assembleAndroidTest",
+                rootProjectFolderPath, rootProjectFolderPath);
+        String compileResult = ProcessRunner.runCommand(compileCmd);
+        if (!compileResult.contains("BUILD SUCCESSFUL")) {
+            throw new Exception("Unable to compile Espresso Tests");
+        }
+
+        // disable animations on emulator
+        ProcessRunner.runCommand("adb shell settings put global window_animation_scale 0");
+        ProcessRunner.runCommand("adb shell settings put global transition_animation_scale 0");
+        ProcessRunner.runCommand("adb shell settings put global animator_duration_scale 0");
+
+        // find where is androidTest apk
+        String findApkCmd = String.format("find %sapp/build/outputs/apk/androidTest/ -name *androidTest.apk",
+                rootProjectFolderPath);
+        String[] apks = ProcessRunner.runCommand(findApkCmd).split("\n");
+        if (apks.length == 0) {
+            throw new Exception("Unable to find compiled Espresso Tests");
+        }
+        String apkTestPath = apks[0];
+
+        // install apk test
+        String installCmd = String.format("adb install %s", apkTestPath);
+        ProcessRunner.runCommand(installCmd);
+
+        // run each test case
+        for (int i = 0; i < espressoTestCases.size(); i++) {
+            String testCaseName = "TestCase" + i;
+
+            String clearCmd = String.format("shell pm clear %s", packageName);
+            ProcessRunner.runCommand(clearCmd);
+
+            String instrumentCmd = String.format("adb shell am instrument -w -r -e emma true -e debug false -e class " +
+                    "%s.%s %s/android.support.test.runner.AndroidJUnitRunner",
+                    testPackageName, testCaseName, testPackageName);
+            String testResult = ProcessRunner.runCommand(instrumentCmd);
+
+            if (!testResult.contains("OK")) {
+                System.out.println("There was an error running test case: " + testCaseName);
+                System.out.println(testResult);
+            }
+        }
+    }
+
+    private static void writeTestCases(String outputFolderPath, List<String> espressoTestCases) throws FileNotFoundException {
+        for (int i = 0; i < espressoTestCases.size(); i++) {
+            String testContent = espressoTestCases.get(i);
+            String outputFilePath = outputFolderPath + "TestCase" + i + ".java";
+
+            PrintWriter out = new PrintWriter(new FileOutputStream(outputFilePath), true);
+            out.print(testContent);
+            out.close();
+        }
+    }
+
+    private static List<TestCase> parseTestCases(String filePath) throws IOException {
+        String content = readFile(filePath, StandardCharsets.US_ASCII);
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(content);
+
+        return TestCaseParser.parseList(mapper, jsonNode);
+    }
+
+    private static String readFile(String path, Charset encoding) throws IOException {
         byte[] encoded = Files.readAllBytes(Paths.get(path));
         return new String(encoded, encoding);
     }
---
+++ src/main/java/org/etg/utils/ProcessRunner.java
@@ -0,0 +1,58 @@
+package org.etg.utils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ProcessRunner {
+    private static final String[] WIN_RUNTIME = { "cmd.exe", "/C" };
+    private static final String[] OS_LINUX_RUNTIME = { "/bin/bash", "-c" };
+
+    private ProcessRunner() {
+    }
+
+    @SuppressWarnings("Since15")
+    private static <T> T[] concat(T[] first, T[] second) {
+        T[] result;
+        result = Arrays.copyOf(first, first.length + second.length);
+        System.arraycopy(second, 0, result, first.length, second.length);
+        return result;
+    }
+
+    public static String runProcess(boolean isWin, String... command) {
+        String[] allCommand = null;
+        try {
+            if (isWin) {
+                allCommand = concat(WIN_RUNTIME, command);
+            } else {
+                allCommand = concat(OS_LINUX_RUNTIME, command);
+            }
+            ProcessBuilder pb = new ProcessBuilder(allCommand);
+            pb.redirectErrorStream(true);
+            Process p = pb.start();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String _temp = null;
+            List<String> line = new ArrayList<String>();
+            while ((_temp = in.readLine()) != null) {
+                line.add(_temp);
+            }
+
+            return String.join("\n", line);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String runCommand(String command){
+        boolean win = false;
+        String os = System.getProperty("os.name");
+        if (os!=null && !os.contains("Linux"))
+            win=true;
+        return ProcessRunner.runProcess(win,command);
+    }
+}
