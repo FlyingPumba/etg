41ed36fc311927780d3b48e0892f7fc8157e27aa
Ivan <iarcuschin@gmail.com>
2019-10-03 11:16:16 -0300

    Minor fix in getApplicationFolderPath method

--- src/main/java/org/etg/ETGProperties.java
+++ src/main/java/org/etg/ETGProperties.java
@@ -59,7 +59,7 @@ public class ETGProperties {
     }
 
     private String getApplicationFolderPath(String rootProjectFolderPath) throws Exception {
-        String grepCmd = String.format("grep -l -R \"apply plugin: 'com.android.application'\" %s", rootProjectFolderPath);
+        String grepCmd = String.format("grep -l -R \"apply plugin: 'com.android.application'\" %s | grep build.gradle", rootProjectFolderPath);
         String[] grepResult = ProcessRunner.runCommand(grepCmd).split("\n");
         if (grepResult.length != 1) {
             throw new Exception("Unable to find application path inside project.");
