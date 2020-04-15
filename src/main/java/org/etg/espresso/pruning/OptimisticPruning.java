b19a9715100aa084f77338b6cdf92ad73c876b5c
Ivan Arcuschin <iarcuschin@gmail.com>
2020-04-15 11:42:16 -0300

    Output size and indexes of failing action clusters

--- src/main/java/org/etg/espresso/pruning/OptimisticPruning.java
+++ src/main/java/org/etg/espresso/pruning/OptimisticPruning.java
@@ -2,7 +2,11 @@ package org.etg.espresso.pruning;
 
 import org.etg.espresso.EspressoTestCase;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -22,9 +26,10 @@ public class OptimisticPruning extends PruningAlgorithm {
     @Override
     public void printSummary(EspressoTestCase espressoTestCase) {
         int widgetActionsCount = espressoTestCase.getWidgetActionsCount();
+        Set<Integer> failingWidgetActionIndexes = espressoTestCase.getFailingWidgetActionIndexes();
         System.out.println(String.format("TEST: %s FAILING-ACTIONS: %d TOTAL-ACTIONS: %d",
                 espressoTestCase.getTestName(),
-                espressoTestCase.getFailingWidgetActionIndexes().size(),
+                failingWidgetActionIndexes.size(),
                 widgetActionsCount));
 
         int lowestFailingWidgetActionIndex = espressoTestCase.getLowestFailingWidgetActionIndex();
@@ -32,5 +37,35 @@ public class OptimisticPruning extends PruningAlgorithm {
                 espressoTestCase.getTestName(),
                 lowestFailingWidgetActionIndex,
                 widgetActionsCount));
+
+        // Order indexes in ascendant order
+        ArrayList<Integer> orderedIndexes = new ArrayList<>(failingWidgetActionIndexes);
+        Collections.sort(orderedIndexes);
+
+        // Get clusters of failing actions
+        int lastFailingIndex = -2;
+        ArrayList<ArrayList<Integer>> failingClusters = new ArrayList<>();
+        for (Integer failingIndex: orderedIndexes) {
+            if (failingIndex != lastFailingIndex + 1) {
+                // this is a new cluster
+                ArrayList<Integer> cluster = new ArrayList<>();
+                cluster.add(failingIndex);
+                failingClusters.add(cluster);
+            } else {
+                // we continue on the same cluster
+                ArrayList<Integer> cluster = failingClusters.get(failingClusters.size() - 1);
+                cluster.add(failingIndex);
+            }
+
+            lastFailingIndex = failingIndex;
+        }
+
+        for (ArrayList<Integer> cluster: failingClusters) {
+            String clusterIndexes = cluster.stream().map(Object::toString).collect(Collectors.joining(","));
+            System.out.println(String.format("TEST: %s FAILING-CLUSTER-SIZE: %d FAILING-CLUSTER-INDEXES: %s",
+                    espressoTestCase.getTestName(),
+                    cluster.size(),
+                    clusterIndexes));
+        }
     }
 }
